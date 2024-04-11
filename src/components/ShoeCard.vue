521fcec72029a83aaee6640fc269b7d1ee0dc884
Eduardo Lazzaretti <duadsal@gmail.com>
2024-04-11 20:19:08 -0300

    ShoeCard

---
+++ src/components/ShoeCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+      :class="`border-2 rounded-xl ${bigShoeImg === imgURL.bigShoe ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`"
+      @click="handleClick"
+    >
+      <div class="flex justify-center items-center bg-gray-200 bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img :src="imgURL.thumbnail" alt="shoe collection" width="127" height="103" class="object-contain" />
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  const { imgURL, changeBigShoeImage, bigShoeImg } = defineProps(['imgURL', 'changeBigShoeImage', 'bigShoeImg']);
+  
+  const handleClick = () => {
+    console.log(bigShoeImg, imgURL.bigShoe);
+    
+      changeBigShoeImage(imgURL.bigShoe);
+    
+  };
+  </script>
+  
--- src/sections/Hero.vue
+++ src/sections/Hero.vue
@@ -1,44 +1,52 @@
 <template>
-    <section id="home" class="w-full flex xl:flex-row flex-col justify-center 
-    min-h-screen gap-10 max-container">
-        <div class="relative xl:w-2/5 flex flex-col justify-center items-start 
-        w-full max-xl:padding-x pt-28">
+    <section id="home" class="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+        <div class="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
             <p class="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
-            <h1 class="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
-                <span class="xl:bg-white xl:whitespace-nowrap relative z-10
-                pr-10">The New Arrival</span>
+            <h1 class="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+                <span class="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
                 <br />
                 <span class="text-coral-red inline-block mt-3">Nike</span> Shoes
             </h1>
-            <p class="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike
-                arrivals,
-                quality comfort, and innovattion for your active life.
+            <p class="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+                Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
             </p>
-            <Button label="Shop Now" :iconURL=iconarrowRight />
-            <div class="flex justify-start items-start flex-wrap w-full
-            mt-20 gap-16 ">
-                <div v-for="(item, index) in statistics" :key="index">
-                    <p class="text-4xl font-palanquin font-bold">{{ item.value }}</p>
-                    <p class="left-7 font-montserrat text-slate-gray">{{ item.label }}</p>
+            <Button label="Shop Now" :iconURL="iconarrowRight" />
+            <div class="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+                <div v-for="(stat, index) in statistics" :key="index">
+                    <p class="text-4xl font-palanquin font-bold">{{ stat.value }}</p>
+                    <p class="leading-7 font-montserrat text-slate-gray">{{ stat.label }}</p>
                 </div>
             </div>
         </div>
-        <div class="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero
-         bg-cover bg-center ">
-            <img :src=imageBigShoe1 alt="" 
-            width="610" height="500" class="object-contain relative z-10"/>
-        </div>
-        <div>
-            
+
+        <div
+            class="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+
+            <img :src="bigShoeImg" alt="shoe collection" width="610" height="502"
+                class="object-contain relative z-10" />
+
+            <div class="flex justify-center items-center sm:gap-6 gap-4 absolute -bottom-[5%]  max-sm:px-6">
+                <ShoeCard v-for="(shoe, index) in shoes" :key="index" :imgURL="shoe"
+                    :changeBigShoeImage="changeBigShoeImage" :bigShoeImg="bigShoeImg" />
+            </div>
         </div>
     </section>
 </template>
+
 <script setup>
-import { statistics } from '@/constants';
-import { arrowRight } from '@/assets/icons'
-import Button from '../components/Button.vue'
-import {bigShoe1} from '@/assets/images' 
+import { ref } from 'vue';
+import { statistics, shoes } from '@/constants';
+import { arrowRight } from '@/assets/icons';
+import Button from '../components/Button.vue';
+import ShoeCard from '@/components/ShoeCard.vue';
+import { bigShoe1 } from '@/assets/images';
 
 const iconarrowRight = arrowRight;
-const imageBigShoe1 = bigShoe1;
+
+const bigShoeImg = ref(bigShoe1);
+
+const changeBigShoeImage = (shoe) => {
+    bigShoeImg.value = shoe;
+};
+
 </script>
